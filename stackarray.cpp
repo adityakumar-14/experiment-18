9c88905dfdb309d5d1fd8905c8f1e56034607ae7
adityakumar-14 <hellx1405@gmail.com>
2024-10-16 11:30:08 +0530

    Create stackarray.cpp

---
+++ stackarray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int> st;
+
+    // Push elements onto the stack
+    st.push(30);
+    st.push(20);
+    st.push(10);
+
+    // Print the top element of the stack
+    cout << "Top element of the stack: " << st.top() << endl;
+
+    return 0;
+}
